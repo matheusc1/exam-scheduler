89e4be04e40dc402c1653dcf33cdf2e21db406c2
Matheus Cardoso <cardoso.matheusbs@gmail.com>
2024-10-21 21:21:23 -0300

    feat: create login page layout

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,3 +1,42 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 export function SignIn() {
-  return <div>Sign in</div>
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="p-10 space-y-5 rounded-md bg-zinc-100 dark:bg-zinc-900">
+        <div className="flex flex-col gap-3 items-center">
+          <h2 className="bg-gradient-to-r from-[#2CACDD] to-[#0FB091] text-transparent bg-clip-text font-bold text-2xl">
+            UNIFAA
+          </h2>
+          <p className="font-semibold">Login - Agendamento de avaliação</p>
+        </div>
+
+        <div className="space-y-3">
+          <div className="space-y-1">
+            <Label>Usuário</Label>
+            <Input
+              className="w-full"
+              type="text"
+              placeholder="Informe seu RA, E-mail ou CPF"
+            />
+          </div>
+
+          <div className="space-y-1">
+            <Label>Senha</Label>
+            <Input
+              className="w-full"
+              type="password"
+              placeholder="Informe sua senha"
+            />
+          </div>
+        </div>
+
+        <Button className="w-full bg-[#0FB091] hover:bg-[#0C926A]">
+          Acessar
+        </Button>
+      </div>
+    </div>
+  )
 }
