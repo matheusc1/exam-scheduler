f394d2658a62e4348c54774de11460811b05e983
Matheus Cardoso <cardoso.matheusbs@gmail.com>
2024-11-12 20:23:07 -0300

    build: install and configure TanStack Query for data fetching

--- src/app.tsx
+++ src/app.tsx
@@ -4,13 +4,17 @@ import { router } from './routes'
 import { ThemeProvider } from './components/theme/theme-provider'
 import { Toaster } from './components/ui/toaster'
 import { AuthProvider } from './context/authContext'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 export function App() {
   return (
     <AuthProvider>
       <ThemeProvider>
         <Toaster />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </AuthProvider>
   )
