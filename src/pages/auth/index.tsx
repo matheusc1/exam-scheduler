10d23340947548ee49fccb610b286c396dda8078
Matheus Cardoso <cardoso.matheusbs@gmail.com>
2024-11-11 21:13:02 -0300

    feat: add authentication

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,8 +1,52 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { useAuth } from '@/context/authContext'
+import { useNavigate } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { api } from '@/lib/axios'
+import { useToast } from '@/hooks/use-toast'
+
+const signInForm = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
+  const { toast } = useToast()
+  const { login } = useAuth()
+  const navigate = useNavigate()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>()
+
+  async function handleLogin(formData: SignInForm) {
+    try {
+      const { data } = await api.post('/login', {
+        email: formData.email,
+        password: formData.password,
+      })
+
+      login(data.role, data.id)
+
+      if (data.role === 'admin') navigate('/admin')
+      if (data.role === 'coordinator') navigate('/coordination')
+      if (data.role === 'student') navigate('/')
+    } catch (err) {
+      toast({
+        variant: 'destructive',
+        title: 'Erro ao fazer login',
+        description: 'E-mail ou senha inválidos',
+      })
+    }
+  }
+
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="p-10 space-y-5 rounded-md bg-zinc-100 dark:bg-zinc-900">
@@ -13,29 +57,39 @@ export function SignIn() {
           <p className="font-semibold">Login - Agendamento de avaliação</p>
         </div>
 
-        <div className="space-y-3">
-          <div className="space-y-1">
-            <Label>Usuário</Label>
-            <Input
-              className="w-full"
-              type="email"
-              placeholder="Informe seu e-mail"
-            />
+        <form className="space-y-5" onSubmit={handleSubmit(handleLogin)}>
+          <div className="space-y-3">
+            <div className="space-y-1">
+              <Label>Usuário</Label>
+              <Input
+                className="w-full"
+                type="email"
+                placeholder="Informe seu e-mail"
+                {...register('email')}
+                required
+              />
+            </div>
+
+            <div className="space-y-1">
+              <Label>Senha</Label>
+              <Input
+                className="w-full"
+                type="password"
+                placeholder="Informe sua senha"
+                {...register('password')}
+                required
+              />
+            </div>
           </div>
 
-          <div className="space-y-1">
-            <Label>Senha</Label>
-            <Input
-              className="w-full"
-              type="password"
-              placeholder="Informe sua senha"
-            />
-          </div>
-        </div>
-
-        <Button className="w-full bg-[#0FB091] hover:bg-[#0C926A]">
-          Acessar
-        </Button>
+          <Button
+            disabled={isSubmitting}
+            type="submit"
+            className="w-full bg-[#0FB091] hover:bg-[#0C926A]"
+          >
+            Acessar
+          </Button>
+        </form>
       </div>
     </div>
   )
