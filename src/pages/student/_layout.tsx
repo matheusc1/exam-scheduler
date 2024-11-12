a419364d3f576a797c8d6f27341e9223248b853e
Matheus Cardoso <cardoso.matheusbs@gmail.com>
2024-11-11 21:14:40 -0300

    feat: add authentication check to layout page

--- src/pages/student/_layout.tsx
+++ src/pages/student/_layout.tsx
@@ -1,7 +1,20 @@
-import { Header } from "@/components/header/header";
-import { Outlet } from "react-router-dom";
+import { Header } from '@/pages/student/components/header/header'
+import { useAuth } from '@/context/authContext'
+import { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 export function Layout() {
+  const navigate = useNavigate()
+  const { isLoading, isAuthenticated, role } = useAuth()
+
+  useEffect(() => {
+    if (isLoading) return
+
+    if (!isAuthenticated || role !== 'student') {
+      navigate('/login', { replace: true })
+    }
+  }, [isLoading, isAuthenticated, role, navigate])
+
   return (
     <div className="max-w-app mx-auto antialiased px-2 sm:px-0">
       <Header />
